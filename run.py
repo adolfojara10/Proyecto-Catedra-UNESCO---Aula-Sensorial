8bbdf873e24986de4517458a3984d9f742a4f442
Adolfo Jara <gavilanesadolfo@gmail.com>
2021-12-24 11:47:57 -0500

    Creado los template de login, docente, alumno

--- run.py
+++ run.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from forms import RegistrarDocente, RegistrarEstudiante, LoginForm
 #from AulaSensorial import db, login_manager
 #from flask import current_app
 from flask_login import UserMixin
 #from itsdangerous import TimedJSONWebSignatureSerializer as serializer
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -40,6 +41,7 @@ class Estudiante(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(75), nullable=False)
     apellido = db.Column(db.String(75), nullable=False)
+    genero = db.Column(db.String(15), nullable=False)
     fechaNacimiento = db.Column(db.Date, nullable=False)
     diagnostico = db.Column(db.String(125), nullable=True)
     residencia = db.Column(db.String(150), nullable=False)
@@ -70,7 +72,28 @@ class Reporte(db.Model):
     def __repr__(self) -> str:
         return f"Docente('{self.docente_id.nombre}', '{self.estudiante_id.nombre}', '{self.juego_id.nombre}', '{self.calificacion}')"
     
-    
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    form = LoginForm()
+       
+    
+    return render_template('login.html', form=form)
+
+@app.route('/registrarDocente')
+def registrarDocente():
+    form = RegistrarDocente()
+    
+    
+    return render_template('registroDocente.html', form=form)
+
+@app.route('/registrarEstudiante')
+def registrarEstudiante():
+    form = RegistrarEstudiante()
+    
+    
+    return render_template('registroEstudiante.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- run.sh
+++
@@ -1,3 +0,0 @@
-set FLASK_APP=principal.py
-set FLASK_DEBUG=1
-flask run
